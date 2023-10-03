cfacb8abee7ab26348113bc0fc176bfb35517c32
Michael Hicks <hicks.michael04@gmail.com>
2023-10-03 09:43:39 -0600

    init

---
+++ main.rb
@@ -0,0 +1,26 @@
+# Problem Solving Process
+# Restate the goal. 
+# Consider the data.
+# Ask clarifying questions.
+# Break it down.
+# Research.
+# Start coding. 
+# Stuck? Go back to your pseudocode.
+# Refactor.
+
+# Challenge 1
+# Write a method or function that accepts a string. The method or function should return a boolean that describes whether or not the string has an even number of characters.
+def str(x)
+    if x.length    
+end
+# Challenge 2
+# Start with an array of strings. Then, print only the words with more than 3 characters.
+
+# Challenge 3
+# Write a method or function that takes an array of numbers as an argument.  The method or function should return the average of all the numbers, rounded to two decimal places.
+
+# Challenge 4
+# Start with an array of strings. Print all of the words in the array, that include the letter t or T.
+
+# Challenge 5
+# Write a method or function that accepts an array of 10 integers (between 0 and 9) and returns a string of those numbers in the form of a phone number. For example, if you were given [5, 5, 5, 1, 2, 3, 4, 5, 6, 7], the method or function will return 555-123-4567.
